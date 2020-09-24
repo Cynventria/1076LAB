e6de1afa6eb5905a5c7cd1ba1388e87b5d6f1454
Cynventria <45027522+Cynventria@users.noreply.github.com>
2020-09-24 05:58:25 +0100

    Add files via upload

---
+++ p092401.py
@@ -0,0 +1,29 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BOARD)
+LED_PIN = 12
+GPIO.setup(LED_PIN, GPIO.OUT)
+
+while True:
+    for i in xrange(0, 3):
+        print('on')
+        GPIO.output(LED_PIN, GPIO.HIGH)
+        time.sleep(0.5)
+        print('off')
+        GPIO.output(LED_PIN, GPIO.LOW)
+        time.sleep(0.5)
+    for i in xrange(0, 3):
+        GPIO.output(LED_PIN, GPIO.HIGH)
+        time.sleep(1)
+        GPIO.output(LED_PIN, GPIO.LOW)
+        time.sleep(1)
+    for i in xrange(0, 3):
+        GPIO.output(LED_PIN, GPIO.HIGH)
+        time.sleep(0.5)
+        GPIO.output(LED_PIN, GPIO.LOW)
+        time.sleep(0.5)
+
+        
+
+GPIO.cleanup();
